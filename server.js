c54bd42f20ee17fa0575fd073618d2389920d7aa
Duc Tram <tramduc1996@yahoo.com>
2018-12-19 13:43:32 -0800

    wip

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const routes = require("./server/routes");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 const port = process.env.PORT || 5050;
@@ -20,9 +21,11 @@ app.get("/", (req, res) => {
   res.send(`PORT: ${port}`);
 });
 
-app.use(express.static(__dirname + "/public"));
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "client/build")));
+// Anything that doesn't match the above, send back index.html
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
