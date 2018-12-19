15ed1e441bdd33006a9d95ab831e7bb0919bedb2
Duc Tram <tramduc1996@yahoo.com>
2018-12-19 13:19:01 -0800

    Update

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.get("/", (req, res) => {
 });
 
 // Serve static files from the React frontend app
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "client/public"));
 // Anything that doesn't match the above, send back index.html
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "client/public/index.html");
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
